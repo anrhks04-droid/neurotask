4aedf77a597e83c36b25d16a8964dc58712384ef
anrhks04-droid <anrhks04-droid@users.noreply.github.com>
2026-04-14 22:58:36 +0900

    Fix: Disable service worker for fresh load

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(
-      (reg) => console.log('ServiceWorker registered:', reg.scope),
-      (err) => console.log('ServiceWorker registration failed:', err)
-    );
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
